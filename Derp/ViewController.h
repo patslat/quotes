3a4694d4b38739725925e48850a254354de37cf2
Patrick Slattery <slattery.patrickj@gmail.com>
2013-08-29 13:55:29 -0700

    done

--- Derp/ViewController.h
+++ Derp/ViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+	@property (nonatomic, strong) NSArray *myQuotes;
+	@property (nonatomic, strong) NSMutableArray *movieQuotes;
+	@property (nonatomic, strong) IBOutlet UITextView *quoteText;
+	@property (nonatomic, strong) IBOutlet UISegmentedControl *quoteOpt;
 
+	- (IBAction)quoteButtonTapped:(id)sender;
 @end
--- Derp/ViewController.m
+++ Derp/ViewController.m
@@ -18,6 +18,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	self.myQuotes = @[
+						@"Live and let live",
+						@"Don't cry over spilt milk",
+						@"Always look on the bright side of life",
+						@"Nobody's perfect",
+						@"Can't see the woods for the trees",
+						@"Better to have loved and lost than not loved at all",
+						@"The early bird catches the worm",
+						@"As slow as a wet week"
+						];
+
+	NSString *plistCatPath = [
+		[NSBundle mainBundle]
+		pathForResource:@"quotes"
+				 ofType:@"plist"
+	];
+    self.movieQuotes = [NSMutableArray arrayWithContentsOfFile:plistCatPath];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +43,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)quoteButtonTapped:(id)sender {
+	NSString *selectedCategory = nil;
+
+	if (self.quoteOpt.selectedSegmentIndex == 0) {
+		selectedCategory = @"modern";
+	} else if (self.quoteOpt.selectedSegmentIndex == 1) {
+		selectedCategory = @"classic";
+	} else {
+		selectedCategory = @"self";
+	}
+	
+	NSPredicate *predicate = [
+		NSPredicate predicateWithFormat:@"category == %@",
+		selectedCategory
+	];
+
+	NSArray *filteredArray = [self.movieQuotes filteredArrayUsingPredicate:predicate];
+	int array_count = [filteredArray count];
+
+	if (array_count > 0) {
+		int index = (arc4random() % array_count);
+		NSString *quote = filteredArray[index][@"quote"];
+		NSString *source = [filteredArray[index] valueForKey:@"source"];
+		if (![source length] == 0) {
+			quote = [NSString stringWithFormat:@"%@\n\n(%@)", quote, source];
+		}
+
+		if ([selectedCategory isEqualToString:@"classic"]) {
+			quote = [NSString stringWithFormat:@"From Classic Movie\n\n%@", quote];
+		} else if ([selectedCategory isEqualToString:@"modern"]) {
+			quote = [NSString stringWithFormat:@"Movie Quote:\n\n%@", quote];
+		} else {
+			if ([source hasPrefix: @"Harry"]){
+				quote = [NSString stringWithFormat:@"Harry Sucks!!\n\n%@", quote];
+			}
+			quote = [NSString stringWithFormat:@"My Quote:\n\n%@", quote];
+		}
+		self.quoteText.text = quote;
+	} else {
+		self.quoteText.text = [NSString stringWithFormat:@"No quotes to display."];
+	}
+}
 @end
